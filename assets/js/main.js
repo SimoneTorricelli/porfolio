8228e94b8c5a5771571af815c3cb1f02c3635edd
st193551 <simone.torricelli.00@gmail.com>
2022-01-25 11:44:43 +0100

    Modale funzionante e piccole modifiche generali

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const  navMenu = document.getElementById('nav-menu'),
+const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu');
     })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', ()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu');
     })
 }
@@ -22,7 +22,7 @@ if(navClose){
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu');
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu');
@@ -36,11 +36,11 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 function toggleSkills() {
     let itemClass = this.parentNode.className;
 
-    for(i = 0; i < skillsContent.length; i++){
+    for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills__content skills__close';
     }
 
-    if(itemClass === 'skills__content skills__close'){
+    if (itemClass === 'skills__content skills__close') {
         this.parentNode.className = 'skills__content skills__open';
     }
 }
@@ -53,17 +53,17 @@ skillsHeader.forEach((el) => {
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab =>{
-    tab.addEventListener('click', ()=>{
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent =>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove('qualification__active');
         });
 
         target.classList.add('qualification__active');
 
-        tabs.forEach(tab =>{
+        tabs.forEach(tab => {
             tab.classList.remove('qualification__active');
         })
         tab.classList.add('qualification__active');
@@ -75,19 +75,19 @@ const modalViews = document.querySelectorAll('.services__modal'),
     modalBtns = document.querySelectorAll('.services__button'),
     modalCloses = document.querySelectorAll('.services__modal-close');
 
-let modal = function (modalClick){
+let modal = function (modalClick) {
     modalViews[modalClick].classList.add('active-modal');
 };
 
-modalBtns.forEach((modalBtn, i)=>{
-    modalBtn.addEventListener('click', ()=>{
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
         modal(i);
     })
 });
 
 modalCloses.forEach(modalClose => {
-    modalClose.addEventListener('click', () =>{
-        modalViews.forEach((modalView) =>{
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal');
         });
     });
@@ -113,17 +113,17 @@ let swiper = new Swiper(".portfolio__container", {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset;
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id');
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
         }
     })
@@ -132,19 +132,19 @@ window.addEventListener('scroll', scrollActive);
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-function scrollHeader(){
+function scrollHeader() {
     const nav = document.getElementById('header');
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
 
 
 /*==================== SHOW SCROLL UP ====================*/
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
 
@@ -187,10 +187,37 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img,.section__title',{delay: 200}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.about, .work__img, .contact__input',{interval: 200}); 
-sr.reveal('.about__info, .about__buttons',{interval: 400}); 
-sr.reveal('.qualification__tabs',{interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input,.services__container,.portfolio__container ',{interval: 300}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img,.section__title', { delay: 200 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.about, .work__img, .contact__input', { interval: 200 });
+sr.reveal('.about__info, .about__buttons', { interval: 400 });
+sr.reveal('.qualification__tabs', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input,.services__container,.portfolio__container ', { interval: 300 });
+
+
+
+// Get the modal
+var modalMessage = document.getElementById("success-modal");
+
+// Get the button that opens the modal
+var submitButton = document.getElementById("submitButton");
+
+
+
+closeButton = document.getElementById("success__modal-button");
+closeButton.addEventListener("click", () => {
+    modalMessage.classList.add("hide-modal")
+});
+
+// When the user clicks on the button, open the modal
+submitButton.onclick = function() {
+    modalMessage.style.display = "flex";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalMessage) {
+    modalMessage.style.display = "none";
+  }
+}
